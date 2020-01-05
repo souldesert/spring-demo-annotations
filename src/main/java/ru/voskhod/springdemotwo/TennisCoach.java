30067fbd8cffd7ea6801fc8be2cc5e1d203e28c4
Александр Васильков <y915@yandex.ru>
2020-01-05 14:17:00 +0300

    Default bean id

--- src/main/java/ru/voskhod/springdemotwo/TennisCoach.java
+++ src/main/java/ru/voskhod/springdemotwo/TennisCoach.java
@@ -2,7 +2,7 @@ package ru.voskhod.springdemotwo;
 
 import org.springframework.stereotype.Component;
 
-@Component("thatSillyCoach")
+@Component
 public class TennisCoach implements Coach {
     @Override
     public String getDailyWorkout() {
