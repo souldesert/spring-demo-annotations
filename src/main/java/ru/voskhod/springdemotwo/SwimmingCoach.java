343627e5b9009b283cd89d235a14aa64a1e7312d
Александр Васильков <y915@yandex.ru>
2020-01-05 16:13:52 +0300

    Injecting properties file using Java annotation

--- src/main/java/ru/voskhod/springdemotwo/SwimmingCoach.java
+++ src/main/java/ru/voskhod/springdemotwo/SwimmingCoach.java
@@ -2,10 +2,25 @@ package ru.voskhod.springdemotwo;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class SwimmingCoach implements Coach {
+
+    @Value("${foo.email}")
+    private String email;
+
+    @Value("${foo.team}")
+    private String team;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getTeam() {
+        return team;
+    }
 
     @Override
     public String getDailyWorkout() {
