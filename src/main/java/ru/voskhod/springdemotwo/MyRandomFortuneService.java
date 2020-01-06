5edbc7c47fa4db27b04b44e550f5669a22cc5cde
Александр Васильков <y915@yandex.ru>
2020-01-06 15:12:07 +0300

    Practice Activity #6 - Bean Scopes with Annotations

--- src/main/java/ru/voskhod/springdemotwo/MyRandomFortuneService.java
+++ src/main/java/ru/voskhod/springdemotwo/MyRandomFortuneService.java
@@ -4,36 +4,50 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 import java.util.Random;
 
 @Component
 public class MyRandomFortuneService implements FortuneService {
 
-    private String fortuneOne;
+    private String[] fortunes = new String[3];
 
-    private String fortuneTwo;
+//    @Autowired
+//    public MyRandomFortuneService(
+//            @Value("${fortune.one}") String fortuneOne,
+//            @Value("${fortune.two}") String fortuneTwo,
+//            @Value("${fortune.three}") String fortuneThree
+//    ) {
+//        this.fortuneOne = fortuneOne;
+//        this.fortuneTwo = fortuneTwo;
+//        this.fortuneThree = fortuneThree;
+//    }
 
-    private String fortuneThree;
+    @PostConstruct
+    public void readFortunes() {
+        try (InputStream input = this.getClass().getClassLoader().getResourceAsStream("sport.properties")) {
+            Properties properties = new Properties();
+            if (input != null) {
+                properties.load(input);
+            } else {
+                throw new FileNotFoundException();
+            }
 
-    @Autowired
-    public MyRandomFortuneService(
-            @Value("${fortune.one}") String fortuneOne,
-            @Value("${fortune.two}") String fortuneTwo,
-            @Value("${fortune.three}") String fortuneThree
-    ) {
-        this.fortuneOne = fortuneOne;
-        this.fortuneTwo = fortuneTwo;
-        this.fortuneThree = fortuneThree;
+            fortunes[0] = properties.getProperty("fortune.one");
+            fortunes[1] = properties.getProperty("fortune.two");
+            fortunes[2] = properties.getProperty("fortune.three");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public String getDailyFortune() {
-        String[] fortunes = {
-                fortuneOne,
-                fortuneTwo,
-                fortuneThree
-        };
-
         return fortunes[new Random().nextInt(fortunes.length)];
     }
 }
