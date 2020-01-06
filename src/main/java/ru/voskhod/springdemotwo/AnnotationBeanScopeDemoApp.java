242eb3021382a207f923f44c337233adbaa19bed
Александр Васильков <y915@yandex.ru>
2020-01-06 14:33:48 +0300

    Scope Annotation

---
+++ src/main/java/ru/voskhod/springdemotwo/AnnotationBeanScopeDemoApp.java
@@ -0,0 +1,23 @@
+package ru.voskhod.springdemotwo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class AnnotationBeanScopeDemoApp {
+    public static void main(String[] args) {
+        // load spring config file
+        ClassPathXmlApplicationContext context =
+                new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        // retrieve beans from spring container
+        Coach coach = context.getBean("tennisCoach", Coach.class);
+        Coach alphaCoach = context.getBean("tennisCoach", Coach.class);
+
+        // check if they are the same
+        boolean result = (coach == alphaCoach);
+        System.out.println("Pointing to the same object: " + result);
+        System.out.println("Memory location for coach: " + coach);
+        System.out.println("Memory location for alphaCoach: " + alphaCoach);
+
+        // close context
+    }
+}
--- src/main/java/ru/voskhod/springdemotwo/TennisCoach.java
+++ src/main/java/ru/voskhod/springdemotwo/TennisCoach.java
@@ -2,32 +2,19 @@ package ru.voskhod.springdemotwo;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
+@Scope("prototype")
 public class TennisCoach implements Coach {
-
-
-    @Autowired
-    @Qualifier("happyFortuneService")
+    
     private FortuneService fortuneService;
 
-    // default constructor
-
-    public TennisCoach() {
-        System.out.println(">> TennisCoach: inside default constructor");
+    @Autowired
+    public TennisCoach(@Qualifier("happyFortuneService") FortuneService fortuneService) {
+        this.fortuneService = fortuneService;
     }
-
-//    @Autowired
-//    public void doSomeCrazyStuff(FortuneService fortuneService) {
-//        System.out.println(">> TennisCoach: inside doSomeCrazyStuff");
-//        this.fortuneService = fortuneService;
-//    }
-
-    //    @Autowired
-//    public TennisCoach(FortuneService fortuneService) {
-//        this.fortuneService = fortuneService;
-//    }
 
     @Override
     public String getDailyWorkout() {
